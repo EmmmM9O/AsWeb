43109b22620644ca0fba211b737d86794126eee4
EmmmPad <2124363741@qq.com>
2023-01-13 07:28:09 +0800

    start

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+      // 启用了类型推导
+    props: {
+        msg: String
+    },
+    data(){
+        return(
+            {
+                msg:'hello world'
+            }
+        )
+    }
+});
+</script>
+<template>
+    <h1>{{msg}}</h1>
+    <input v-model="msg">
+</template>
---
+++ src/vue/Nav.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { defineComponent, readonly } from 'vue';
+interface Page{
+    readonly name:String,
+    readonly url:String
+}
+export default defineComponent({
+    props:{
+        PageList:Array<Page>
+    }
+});
+</script>
